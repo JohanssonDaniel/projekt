4dd0c61f50af81ccc0c0d6d6567b192bd7cd3f43
hjorten911 <hjorten_911@hotmail.com>
2015-04-15 10:20:20 +0200

    fixed up, down and gameover

--- projectAlgorithm/main.cpp
+++ projectAlgorithm/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
+
 int field[4][4];
 static const int MAXROW = 4;
 static const int MAXCOL = 4;
 static const int MAXSIZE = 4;
 static const int outside_field_right = 4;
 static const int outside_field_left = -1;
+static const int outside_field_down = 4;
+static const int outside_field_up = -1;
 bool hasMoved = false;
 using namespace std;
 
@@ -41,6 +45,30 @@ bool canMergeRight(int row, int current_col){
     }
 }
 
+bool canMergeDown(int current_row, int col){
+
+    //cout << "currentVal: " << currentVal << endl;
+    for(int i = current_row; i < MAXROW - 1; i++){
+        int currentVal = field[current_row][col];
+        int neighbourVal = field[current_row+1][col];
+        //Nästa granne till höger är 0
+        if (field[current_row+1][col] == 0){
+            field[current_row][col] = 0;
+            field[current_row+1][col] = currentVal;
+            //Vi fortsätter följa kolumnen åt höger
+            current_row++;
+        }
+        else if(currentVal == neighbourVal){
+            field[current_row][col] = 0;
+            field[current_row+1][col] += currentVal;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
 bool canMergeLeft(int row, int current_col){
 
     //cout << "currentVal: " << currentVal << endl;
@@ -57,6 +85,30 @@ bool canMergeLeft(int row, int current_col){
         else if(currentVal == neighbourVal){
             field[row][current_col] = 0;
             field[row][current_col - 1] += currentVal;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+bool canMergeUp(int current_row, int col){
+
+    //cout << "currentVal: " << currentVal << endl;
+    for(int i = current_row; i > 0; i--){
+        int currentVal = field[current_row][col];
+        int neighbourVal = field[current_row-1][col];
+        //Nästa granne till höger är 0
+        if (field[current_row-1][col] == 0){
+            field[current_row][col] = 0;
+            field[current_row-1][col] = currentVal;
+            //Vi fortsätter följa kolumnen åt höger
+            current_row--;
+        }
+        else if(currentVal == neighbourVal){
+            field[current_row][col] = 0;
+            field[current_row-1][col] += currentVal;
             return true;
         }
         else{
@@ -89,6 +141,30 @@ void switchRight(){
     }
 }
 
+void switchDown() {
+    //cout << "Switching field right..." << endl;
+    for(int col = 0; col < MAXCOL; col++){
+        int current_row = MAXROW -2;
+        while(current_row != outside_field_up){
+            int field_val = field[current_row][col];
+            //cout << "field_val: " << field_val << endl;
+            if (field_val == 0){
+                if (current_row == 0){
+                    break;
+                }else{
+                    current_row--;
+                }
+            }
+            else if(canMergeDown(current_row,col)){
+                hasMoved = true;
+                current_row--;
+            }else{
+                current_row--;
+            }
+        }
+    }
+}
+
 void switchLeft() {
     //cout << "Switching field right..." << endl;
     for(int row = 0; row < MAXROW; row++){
@@ -113,6 +189,32 @@ void switchLeft() {
     }
 }
 
+void switchUp() {
+    //cout << "Switching field right..." << endl;
+    for(int col = 0; col < MAXCOL; col++){
+        int current_row = 1;
+        while(current_row != outside_field_down){
+            int field_val = field[current_row][col];
+            //cout << "field_val: " << field_val << endl;
+            if (field_val == 0){
+                if (current_row == 3){
+                    break;
+                }else{
+                    current_row++;
+                }
+            }
+            else if(canMergeUp(current_row,col)){
+                hasMoved = true;
+                current_row++;
+            }else{
+                current_row++;
+            }
+        }
+    }
+}
+
+
+
 void randomInsertTwoOrFour() {
     int twoOrFourList[5] = {2,2,2,2,4};
     int randNum = rand() % 5;
@@ -122,8 +224,8 @@ void randomInsertTwoOrFour() {
 
     bool hasNotInserted = true;
     while(hasNotInserted) {
-        int randCol = rand() % 3;
-        int randRow = rand() % 3;
+        int randCol = rand() % 4;
+        int randRow = rand() % 4;
 
         if (field[randRow][randCol] == 0) {
             field[randRow][randCol] = randValue;
@@ -133,29 +235,33 @@ void randomInsertTwoOrFour() {
 }
 
 bool gameOver(){
-/*    for (int row = 0; row < grid.numRows(); ++row) {
-        for (int col = 0; col < grid.numCols(); ++col) {
-            currentVal = field[row][col];
+    for (int row = 0; row < MAXROW; ++row) {
+        for (int col = 0; col < MAXCOL; ++col) {
+            int currentVal = field[row][col];
 
-            // ej klar ^
+            vector<int> colValues {col - 1, /*col, col,*/ col + 1};
+            vector<int> rowValues {row    , /*row + 1 , row - 1,*/ row    };
 
-            vector<int> xValues {col - 1,  col, col, col + 1};
-            vector<int> yValues {row, row + 1, row - 1,row};
-
-            int countAliveNeighbours = 0;
             // Count neighbours algorithm
-            for (int i = 0; i < MAXSIZE; i++) {
-                if (xValues[i] < MAXCOL && xValues[i] >= 0
-                        && yValues[i] < MAXROW && yValues[i] >= 0) {
-                    if (field[yValues[i]][xValues[i]] == 0
-                            || currentVal == field[yValues[i]][xValues[i]]) {
-                        countAliveNeighbours++;
+            // should be MAXSIZE, but is MAXSIZE 2 due to no up, won movement.
+            for (int i = 0; i < MAXSIZE-2; i++) {
+                if (colValues[i] < MAXCOL && colValues[i] >= 0
+                        && rowValues[i] < MAXROW && rowValues[i] >= 0) {
+                    if (field[rowValues[i]][colValues[i]] == 0
+                            || currentVal == field[rowValues[i]][colValues[i]]) {
+                        cout << currentVal << endl;
+                        cout << row << endl;
+                        cout << col << endl;
+                        return true;
+
                     }
                 }
             }
         }
     }
-    */
+    cout << "nooo" << endl;
+    return false;
+
 }
 
 int main()
@@ -184,6 +290,12 @@ int main()
         case 'r':
             switchRight();
             break;
+        case 'u':
+            switchUp();
+            break;
+        case 'd':
+            switchDown();
+            break;
         default:
             break;
         }
@@ -191,11 +303,12 @@ int main()
         if (hasMoved) {
             randomInsertTwoOrFour();
         }
-        /*
-        if (gameOver()){
+
+        if (!gameOver()){
+            cout<<"gg"<<endl;
             break;
         }
-        */
+
 
         printField();
         cout << "Direction: ";
@@ -203,4 +316,3 @@ int main()
     }
     return 0;
 }
-
