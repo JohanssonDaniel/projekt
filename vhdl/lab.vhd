af26872b4eb4c0d575e029c0781c87837ac65d24
JohanssonDaniel <johanssondaniel93@gmail.com>
2015-04-23 22:57:11 +0200

    Flyttade ut algoritmen till flera processer för att bättre efterlikna hur VHDL ska skrivas

--- vhdl/lab.vhd
+++ vhdl/lab.vhd
@@ -20,30 +20,37 @@ entity lab is
 end lab;
 
 architecture Behavioral of lab is
-  component leddriver
+	component leddriver
     Port ( clk,rst : in  STD_LOGIC; 
            ca,cb,cc,cd,ce,cf,cg,dp : out  STD_LOGIC;
            an : out  STD_LOGIC_VECTOR (3 downto 0);
            ledvalue : in  STD_LOGIC_VECTOR (15 downto 0));
-  end component;
+	end component;
   
-	signal movingBlockDirection : STD_LOGIC_VECTOR (1 downto 0) := "00";
-	CONSTANT moveLeft : STD_LOGIC_VECTOR (1 downto 0):= "11";
-	CONSTANT moveRight : STD_LOGIC_VECTOR (1 downto 0):= "10";
-	signal currentColumn : STD_LOGIC_VECTOR (1 downto 0) := "00";
-	signal columnVal : STD_LOGIC_VECTOR (3 downto 0) := "0000";
-	signal neighbuorVal : STD_LOGIC_VECTOR (3 downto 0) := "0000";
-	signal clkctr : STD_LOGIC_VECTOR (1 downto 0) := "00"; 				-- Clock counter (mod 4 in design)
-	signal datactr: STD_LOGIC_VECTOR (1 downto 0) := "00";				-- When recieving data, point to the correct row in pitcure mem
-	signal row : STD_LOGIC_VECTOR (1 downto 0) := "00";	
-	signal sreg : STD_LOGIC_VECTOR (15 downto 0) := "0000000000000000";						-- Register for sending current row to vga
+	CONSTANT direction_left  : STD_LOGIC_VECTOR (3 downto 0)  := "0001";
+	CONSTANT direction_right : STD_LOGIC_VECTOR (3 downto 0)  := "0010";
+	CONSTANT direction_up    : STD_LOGIC_VECTOR (3 downto 0)  := "0011";
+	CONSTANT direction_down  : STD_LOGIC_VECTOR (3 downto 0)  := "0100";
 	
-	type ram_t is array (0 to 3) of std_logic_vector(0 to 15);			--Varje två potens representeras av en byte och vi har fyra på varje ram
+	signal   direction       : STD_LOGIC_VECTOR (3 downto 0)  := "0000";
 	
-	constant notsorandomstart : ram_t := ("0000000000000000",
-										"0000001000000000",
-										"0000000000000000",
-										"0000000000000000");	--Spawns a 2 on (1,1)
+	signal   load_to_mem     : STD_LOGIC					  := "0";
+	signal 	 rowReg          : STD_LOGIC_VECTOR (15 downto 0) := "0000000000000000";
+	signal   colReg is array (0 to 3) of std_logic_vector (0 to 3);
+	signal   neighbuorVal    : STD_LOGIC_VECTOR (3 downto 0)  := "0000";
+	signal 	 colctr, rowctr : STD_LOGIC_VECTOR (1 downto 0) := "00";
+	signal 	 currentColumn, currentRow : STD_LOGIC_VECTOR (1 downto 0) := "00";
+	
+	signal   clkctr       : STD_LOGIC_VECTOR (1 downto 0)              := "00"; 				-- Clock counter (mod 4 in design)
+	signal   srow         : STD_LOGIC_VECTOR (1 downto 0)             := "00";	
+	signal   sreg         : STD_LOGIC_VECTOR (15 downto 0)             := "0000000000000000";	-- Register for sending current row to vga
+	
+	type ram_t is array (0 to 3) of std_logic_vector(0 to 15);			--Varje två potens representeras av en byte och vi har fyra på varje rad
+	
+	constant notsorandomstart : ram_t := ("0000000000100000",
+										  "0000000000000000",
+										  "0000000000000000",
+										  "0000000000000000");	--sätter e 2:a på plats (0,2)
 	signal bildminne,tempRam : ram_t := notsorandomstart;
 	
 begin
@@ -61,9 +68,9 @@ begin
 	process(clk) begin											-- Next row every 4:th clock
 		if rising_edge(clk) then 
 			if rst = '1' then
-				row <= "00";
+				srow <= "00";
 			elsif clkctr = 3 then
-				row <= row + 1;
+				srow <= srow + 1;
 			end if;
 		end if;
 	end process;
@@ -71,10 +78,9 @@ begin
 	process(clk)begin												-- Load next row from ram into sreg 
 		if rising_edge(clk) then
 			if rst = '1' then
-				row <= "00";
 				sreg <= "0000000000000000";
 			elsif clkctr = 3 then
-				sreg <= bildminne(conv_integer(row) + 1);
+				sreg <= bildminne(conv_integer(srow) + 1);
 			end if;
 		end if;
 	end process;
@@ -83,33 +89,82 @@ begin
 	process(clk) begin
 		if rising_edge(clk) then
 			if rst = '1' then
-				--resetboard
-				movingBlockDirection <= "00";
-			elsif JA = 16 then								--Vänster
-				movingBlockDirection <= moveLeft;
-			elsif JA = 8 then							--Höger
-				movingBlockDirection <= moveRight;
+				direction  <= "00";
+			elsif JA = 16 then										--Vänster
+				currentColumn <= "01";
+				direction  <= direction_left;
+			elsif JA = 8 then										--Höger
+				currentColumn <= "10";
+				direction  <= direction_right;
 			end if;
 		end if;
 	end process;
 	
+	-- Testar om vi kommit till slutet av raden/kolumnen
+	process(clk) begin
+		if rising_edge(clk) then
+			case direction is
+				when direction_right => if colctr = -1 then rowctr = rowctr + 1;
+				when direction_left  => if colctr =  4 then rowctr = rowctr + 1;
+				when direction_up    => if rowctr = -1 then colctr = colctr + 1;
+				when direction_down  => if rowctr =  4 then colctr = colctr + 1;				
+				when others => direction <= direction;
+			end case;
+		end if;
+	end process;
+	
+	process(clk) begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				load_to_mem <= 0;
+			elsif load_to_mem = 1 then
+				load_to_mem <= 0;
+				bildminne <= tempRam;
+			end if;
+		end if;
+	end process;
+		
+	process(clk) begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				load_row = '0';
+				load_col = '0';
+				rowReg    <= "0000000000000000";
+				colReg    <= ("0000",
+							  "0000",
+							  "0000",
+							  "0000");
+			elsif load_row = '1' then 
+				rowReg    <= tempRam(conv_integer(row) +1);			--Ladda nuvarande rad 
+			elsif load_col = '1' then
+				colReg    <= tempRam(0 to 3)(conv_integer(currentColumn)*4 to conv_integer(currentColumn)*4 + 4); -- Ladda alla rader i kolumnen
+			end if;
+		end if;
+	end process;
+	
+	process(clk) begin
+		if rising_edge(clk) then
+			
+	
 	--2048
 	process(clk) begin
 		if rising_edge(clk)	then
-			if row = 4	then
-				bildminne <= tempRam;							--Placeholder
-				row <= "00";
+			if gameRow = 4 then
+				bildminne <= tempRam;							--Kommit till sista raden, skriv över bildminnet med det temporära bildminnet
+				gameRow <= "00";
 			elsif currentColumn = -1 then						--Näst sista kolumnen
-				row <= row + 1;
+				gameRow <= gameRow + 1;
 			else
-				columnVal <= tempRam(conv_integer(row) + 1)((conv_integer(currentColumn)*4) to ((conv_integer(currentColumn)+1)*4)-1);
-				if columnVal = 0	then 				--currentColumn index the second bit, currentColumn*4 index second byte	
+				variable rowVal : std_logic_vector (0 to 15) <= tempRam(conv_integer(gameRow) + 1);
+				 -- kolumnen skjusteras för att peka på den 8:e biten istället för den 2:a
+				variable columnVal : std_logic_vector (4 downto 0) <= rowVal(conv_integer(gameColumn)*4 to conv_integer(gameColumn + 1)*4 - 1);
+				if columnVal = 0 then 				-- currentColumn index the second bit, currentColumn*4 index second byte	
 					currentColumn <= currentColumn - 1;
 				elsif currentColumn = 3 then 
-					neighbuorVal <= tempRam(conv_integer(row) + 1)(conv_integer(currentColumn+1)*4 to (conv_integer(currentColumn)+2)*4-1);
+					neighbuorVal <= rowVal(conv_integer(gameColumn + 1)*4 to conv_integer(gameColumn + 2)*4 - 1);
 					if neighbuorVal = 0 then
+						tempRam(conv_integer(row) + 1)(conv_integer(currentColumn)*4 to ((conv_integer(currentColumn)+1)*4)-1) <= "0000";
 						tempRam(conv_integer(row) + 1)(conv_integer(currentColumn+1)*4 to (conv_integer(currentColumn)+2)*4-1) <= columnVal;
-						tempRam(conv_integer(row) + 1)((conv_integer(currentColumn)*4) to ((conv_integer(currentColumn)+1)*4)-1) <= "0000";
 					elsif columnVal = neighbuorVal then
 						tempRam(conv_integer(row) + 1)(conv_integer(currentColumn+1)*4 to (conv_integer(currentColumn)+2)*4-1) <= columnVal + neighbuorVal;
 						tempRam(conv_integer(row) + 1)((conv_integer(currentColumn)*4) to ((conv_integer(currentColumn)+1)*4)-1) <= "0000";
