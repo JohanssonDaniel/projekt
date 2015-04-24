e79d3a60dcaf636839c0554c627c69ece7adf7be
JohanssonDaniel <johanssondaniel93@gmail.com>
2015-04-24 10:01:06 +0200

    bilder samt kompilering av processer

--- vhdl/lab.vhd
+++ vhdl/lab.vhd
@@ -27,23 +27,32 @@ architecture Behavioral of lab is
            ledvalue : in  STD_LOGIC_VECTOR (15 downto 0));
 	end component;
   
-	CONSTANT direction_left  : STD_LOGIC_VECTOR (3 downto 0)  := "0001";
-	CONSTANT direction_right : STD_LOGIC_VECTOR (3 downto 0)  := "0010";
-	CONSTANT direction_up    : STD_LOGIC_VECTOR (3 downto 0)  := "0011";
-	CONSTANT direction_down  : STD_LOGIC_VECTOR (3 downto 0)  := "0100";
+	CONSTANT direction_left   : STD_LOGIC_VECTOR (3 downto 0)  := "0001";
+	CONSTANT direction_right  : STD_LOGIC_VECTOR (3 downto 0)  := "0010";
+	CONSTANT direction_up     : STD_LOGIC_VECTOR (3 downto 0)  := "0011";
+	CONSTANT direction_down   : STD_LOGIC_VECTOR (3 downto 0)  := "0100";
 	
-	signal   direction       : STD_LOGIC_VECTOR (3 downto 0)  := "0000";
+	signal   direction        : STD_LOGIC_VECTOR (3 downto 0)  := "0000";
 	
-	signal   load_to_mem     : STD_LOGIC					  := "0";
-	signal 	 rowReg          : STD_LOGIC_VECTOR (15 downto 0) := "0000000000000000";
-	signal   colReg is array (0 to 3) of std_logic_vector (0 to 3);
-	signal   neighbuorVal    : STD_LOGIC_VECTOR (3 downto 0)  := "0000";
-	signal 	 colctr, rowctr : STD_LOGIC_VECTOR (1 downto 0) := "00";
-	signal 	 currentColumn, currentRow : STD_LOGIC_VECTOR (1 downto 0) := "00";
+	signal   load_to_mem      : STD_LOGIC					  := '0';
+	signal   load_Row         : STD_LOGIC					  := '0';
+	signal   load_Col         : STD_LOGIC					  := '0';
 	
-	signal   clkctr       : STD_LOGIC_VECTOR (1 downto 0)              := "00"; 				-- Clock counter (mod 4 in design)
-	signal   srow         : STD_LOGIC_VECTOR (1 downto 0)             := "00";	
-	signal   sreg         : STD_LOGIC_VECTOR (15 downto 0)             := "0000000000000000";	-- Register for sending current row to vga
+	signal 	 rowReg           : STD_LOGIC_VECTOR (0 to 15) := "0000000000000000";
+	type     column            is array (0 to 3) of std_logic_vector (0 to 3);
+	signal   colReg			  : column 						  :=   ("0000",
+											                        "0000",
+											                        "0000",
+											                        "0000");
+	
+	signal   neighbourVal     : STD_LOGIC_VECTOR (3 downto 0)           := "0000";
+	signal   currentVal		  : STD_LOGIC_VECTOR (3 downto 0)           := "0000";	
+	signal 	 colctr, rowctr   : STD_LOGIC_VECTOR (1 downto 0)           := "00";
+	signal 	 currentColumn, currentRow : STD_LOGIC_VECTOR (1 downto 0)  := "00";
+	
+	signal   clkctr           : STD_LOGIC_VECTOR (1 downto 0)              := "00"; 				-- Clock counter (mod 4 in design)
+	signal   srow             : STD_LOGIC_VECTOR (1 downto 0)              := "00";	
+	signal   sreg             : STD_LOGIC_VECTOR (0 to 15)             := "0000000000000000";	-- Register for sending current row to vga
 	
 	type ram_t is array (0 to 3) of std_logic_vector(0 to 15);			--Varje två potens representeras av en byte och vi har fyra på varje rad
 	
@@ -51,7 +60,7 @@ architecture Behavioral of lab is
 										  "0000000000000000",
 										  "0000000000000000",
 										  "0000000000000000");	--sätter e 2:a på plats (0,2)
-	signal bildminne,tempRam : ram_t := notsorandomstart;
+	signal bildminne,tempRam  : ram_t := notsorandomstart;
 	
 begin
 	--GPU
@@ -104,10 +113,14 @@ begin
 	process(clk) begin
 		if rising_edge(clk) then
 			case direction is
-				when direction_right => if colctr = -1 then rowctr = rowctr + 1;
-				when direction_left  => if colctr =  4 then rowctr = rowctr + 1;
-				when direction_up    => if rowctr = -1 then colctr = colctr + 1;
-				when direction_down  => if rowctr =  4 then colctr = colctr + 1;				
+				when direction_right => if colctr = -1 then rowctr <= rowctr + 1;
+										end if;
+				when direction_left  => if colctr =  4 then rowctr <= rowctr + 1;
+										end if;
+				when direction_up    => if rowctr = -1 then colctr <= colctr + 1;
+										end if;
+				when direction_down  => if rowctr =  4 then colctr <= colctr + 1;				
+										end if;
 				when others => direction <= direction;
 			end case;
 		end if;
@@ -116,62 +129,99 @@ begin
 	process(clk) begin
 		if rising_edge(clk) then
 			if rst = '1' then
-				load_to_mem <= 0;
-			elsif load_to_mem = 1 then
-				load_to_mem <= 0;
+				load_to_mem <= '0';
+			elsif load_to_mem = '1' then
+				load_to_mem <= '0';
 				bildminne <= tempRam;
 			end if;
 		end if;
 	end process;
-		
+	--Ladda rad/kolumn reg	
 	process(clk) begin
 		if rising_edge(clk) then
 			if rst = '1' then
-				load_row = '0';
-				load_col = '0';
+				load_row <= '0';
+				load_col <= '0';
 				rowReg    <= "0000000000000000";
 				colReg    <= ("0000",
 							  "0000",
 							  "0000",
 							  "0000");
 			elsif load_row = '1' then 
-				rowReg    <= tempRam(conv_integer(row) +1);			--Ladda nuvarande rad 
+				rowReg    <= tempRam(conv_integer(rowCtr) +1);														  --Ladda nuvarande rad 
 			elsif load_col = '1' then
-				colReg    <= tempRam(0 to 3)(conv_integer(currentColumn)*4 to conv_integer(currentColumn)*4 + 4); -- Ladda alla rader i kolumnen
+				colReg    <= tempRam(0 to 3)(conv_integer(colctr)*4 to conv_integer(colctr)*4 + 4); 				  -- Ladda alla rader i kolumnen
 			end if;
 		end if;
 	end process;
 	
+	--Om värdet i raden/kolumnen är noll, gå till nästa rad/kolumn
 	process(clk) begin
 		if rising_edge(clk) then
-			
+			case direction is
+				when direction_right|direction_left => currentVal <= rowReg(conv_integer(currentColumn) to conv_integer(currentColumn) + 3);
+				when direction_up|direction_down    => currentVal <= colReg(conv_integer(currentRow));
+				when others                         => direction <= direction;
+			end case;
+		end if;
+	end process;
 	
-	--2048
+	--Om värdet i raden/kolumnen är noll, gå till nästa rad/kolumn
 	process(clk) begin
-		if rising_edge(clk)	then
-			if gameRow = 4 then
-				bildminne <= tempRam;							--Kommit till sista raden, skriv över bildminnet med det temporära bildminnet
-				gameRow <= "00";
-			elsif currentColumn = -1 then						--Näst sista kolumnen
-				gameRow <= gameRow + 1;
-			else
-				variable rowVal : std_logic_vector (0 to 15) <= tempRam(conv_integer(gameRow) + 1);
-				 -- kolumnen skjusteras för att peka på den 8:e biten istället för den 2:a
-				variable columnVal : std_logic_vector (4 downto 0) <= rowVal(conv_integer(gameColumn)*4 to conv_integer(gameColumn + 1)*4 - 1);
-				if columnVal = 0 then 				-- currentColumn index the second bit, currentColumn*4 index second byte	
-					currentColumn <= currentColumn - 1;
-				elsif currentColumn = 3 then 
-					neighbuorVal <= rowVal(conv_integer(gameColumn + 1)*4 to conv_integer(gameColumn + 2)*4 - 1);
-					if neighbuorVal = 0 then
-						tempRam(conv_integer(row) + 1)(conv_integer(currentColumn)*4 to ((conv_integer(currentColumn)+1)*4)-1) <= "0000";
-						tempRam(conv_integer(row) + 1)(conv_integer(currentColumn+1)*4 to (conv_integer(currentColumn)+2)*4-1) <= columnVal;
-					elsif columnVal = neighbuorVal then
-						tempRam(conv_integer(row) + 1)(conv_integer(currentColumn+1)*4 to (conv_integer(currentColumn)+2)*4-1) <= columnVal + neighbuorVal;
-						tempRam(conv_integer(row) + 1)((conv_integer(currentColumn)*4) to ((conv_integer(currentColumn)+1)*4)-1) <= "0000";
-						--currentScore
-					end if;
-				end if;
-			end if;
+		if rising_edge(clk) then
+			case direction is
+				when direction_right => if currentVal = "00" then currentColumn <= currentColumn - 1;
+										end if;
+				when direction_left  => if currentVal = "00" then currentColumn <= currentColumn + 1;
+										end if;
+				when direction_up    => if currentVal = "00" then colctr <= colctr - 1;
+										end if;
+				when direction_down  => if currentVal = "00" then colctr <= colctr + 1;	
+										end if;
+				when others 		=> direction <= direction;
+			end case;
+		end if;
+	end process;
+	
+	--Tar ut grannen radvis/kolumnvis
+	process(clk) begin
+		if rising_edge(clk) then
+			case direction is
+				when direction_right => neighbourVal <= rowReg((conv_integer(currentColumn)*4 + 4) to (conv_integer(currentColumn)*4 + 7));
+				when direction_left  => neighbourVal <= rowReg((conv_integer(currentColumn)*4 - 4) to (conv_integer(currentColumn)*4 - 7));
+				when direction_up    => neighbourVal <= colReg(conv_integer(currentRow) + 1);
+				when direction_down  => neighbourVal <= colReg(conv_integer(currentRow) - 1);
+				when others 		 =>	direction <= direction;
+			end case;
+		end if;
+	end process;
+	
+	--Merge
+	process(clk) begin
+		if rising_edge(clk) then
+			case direction is
+				when direction_right => if    neighbourVal = "0000" then rowReg(conv_integer(currentColumn)*4 to  conv_integer(currentColumn)*4 + 3) <= "0000";
+												                    rowReg(conv_integer(currentColumn)*4 + 4      to conv_integer(currentColumn)*4 + 7) <= currentVal;
+									    elsif neighbourVal = currentVal then rowReg(conv_integer(currentColumn)*4 to conv_integer(currentColumn)*4 + 3) <= "0000";
+												                            rowReg(conv_integer(currentColumn)*4 + 4 to conv_integer(currentColumn)*4 + 7) <= currentVal + neighbourVal; 
+										end if;										
+				when direction_left  => if    neighbourVal = "0000" then rowReg(conv_integer(currentColumn)*4 to  conv_integer(currentColumn)*4) <= "0000";
+												                    rowReg(conv_integer(currentColumn)*4 - 4 to conv_integer(currentColumn)*4 - 7) <= currentVal;
+									    elsif neighbourVal = currentVal then rowReg(conv_integer(currentColumn)*4 to conv_integer(currentColumn)*4 + 3) <= "0000";
+												                            rowReg(conv_integer(currentColumn)*4 + 4 to conv_integer(currentColumn)*4 - 7) <= currentVal + neighbourVal; 
+										end if;
+				when direction_up    => if    neighbourVal = "0000"         then colReg(conv_integer(currentRow))     <= "0000";
+												                            colReg(conv_integer(currentRow) + 1) <= currentVal;
+									    elsif neighbourVal = currentVal then colReg(conv_integer(currentRow))     <= "0000";
+												                            colReg(conv_integer(currentRow) + 1) <= currentVal + neighbourVal; 
+										end if;
+				when direction_down    => if    neighbourVal = "0000"         then colReg(conv_integer(currentRow))   <= "0000";
+												                            colReg(conv_integer(currentRow) - 1) <= currentVal;
+									    elsif neighbourVal = currentVal then colReg(conv_integer(currentRow))     <= "0000";
+												                            colReg(conv_integer(currentRow) - 1) <= currentVal + neighbourVal;
+										end if;
+				when others         => direction <= direction;
+			end case;
 		end if;
 	end process;
 end Behavioral;
